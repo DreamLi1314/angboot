9ca7bdf69496cecd926705ab97ce6ab24fb39943
DreamLi <18829346477@163.com>
2019-04-02 22:03:15 +0800

    init project

---
+++ src/main/java/com/dream/angboot/AngbootApplication.java
@@ -0,0 +1,13 @@
+package com.dream.angboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngbootApplication.class, args);
+    }
+
+}
