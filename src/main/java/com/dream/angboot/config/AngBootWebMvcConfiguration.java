1639907b155819bd4c4fd9252e1d9bf37e845ca0
DreamLi <18829346477@163.com>
2019-05-11 15:16:21 +0800

    mapping /index.html

--- src/main/java/com/dream/angboot/config/AngBootWebMvcConfiguration.java
+++ src/main/java/com/dream/angboot/config/AngBootWebMvcConfiguration.java
@@ -21,8 +21,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AngBootWebMvcConfiguration implements WebMvcConfigurer {
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login.html").setViewName("login");
-    }
+   @Override
+   public void addViewControllers(ViewControllerRegistry registry) {
+      registry.addViewController("/index.html").setViewName("index");
+      registry.addViewController("/login.html").setViewName("login");
+   }
 }
