4ffad36edb2a11f2a1dd54e84b1fa01c6f486ec3
DreamLi <18829346477@163.com>
2019-06-30 10:34:33 +0800

    code review.

--- src/main/java/com/angboot/config/DruidConfiguration.java
+++ src/main/java/com/angboot/config/DruidConfiguration.java
@@ -20,12 +20,10 @@ import com.alibaba.druid.support.http.WebStatFilter;
 import com.angboot.util.AngBootEnv;
 import com.angboot.util.db.DBConfig;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 import java.util.Arrays;
